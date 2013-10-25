e18e63312fe6842d3c4c27aebb1d3410260106aa
Ijwu <catstop123@hotmail.com>
2013-10-24 22:45:23 -0400

    Added Formatter interface and CSVFormatter.

---
+++ Documentation/IFormatter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using TShockAPI;
+using Terraria;
+
+namespace Documentation
+{
+	/// <summary>
+	/// Formats the output for writing docs to files.
+	/// </summary>
+	public interface IFormatter
+	{
+		string FormatWalls();
+		string FormatTiles();
+		string FormatItems();
+		string FormatMobs();
+		string FormatCommands();
+	}
+}
