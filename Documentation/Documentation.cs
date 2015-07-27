cec127eb7a10eab54a98a1a139dfc6975037f4c9
White <monsieur_white@hotmail.com>
2015-07-27 12:55:48 +0930

    1.20 API tick

--- Documentation/Documentation.cs
+++ Documentation/Documentation.cs
@@ -10,7 +10,7 @@ using TShockAPI;
 
 namespace Documentation
 {
-    [ApiVersion(1, 17)]
+    [ApiVersion(1, 20)]
     public class Documentation : TerrariaPlugin
     {
         public override string Author { get { return "White, Ijwu"; } }
